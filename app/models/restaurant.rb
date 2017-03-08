6bbcdc58ce745a772419a805c3596402c45c3e31
Guillaume Salin <gsalin@gmail.com>
2017-03-08 13:59:09 +0100

    added the model Review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: ["chinese", "italian", "french", "japanese", "belgian"], allow_nil: false }, presence: true
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
